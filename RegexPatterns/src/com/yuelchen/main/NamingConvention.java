71531ebf1f3eb5afdaf266a2b315f09429b73424
yuelchen <ychen.workspace@gmail.com>
2020-09-03 00:14:34 -0400

    Updated Java Docs.

--- RegexPatterns/src/com/yuelchen/main/NamingConvention.java
+++ RegexPatterns/src/com/yuelchen/main/NamingConvention.java
@@ -1,7 +1,19 @@
 package com.yuelchen.main;
 
+/**
+ * NamingConvention uses patterns to match against String values. 
+ * 
+ * @author 	yuelchen
+ * @version	1.0.0
+ * @since	2020-09-03
+ */
 public class NamingConvention {
 	
+	/**
+	 * Main method. 
+	 * 
+	 * @param args				an array of String values. 
+	 */
 	public static void main(String[] args) {
 		
 		/* Symbol Expression
@@ -74,6 +86,14 @@ public class NamingConvention {
 	
 	//====================================================================================================
 	
+	/**
+	 * Returns boolean representation of whether or not social security number   
+	 * matches convention.
+	 * 
+	 * @param number			the SSN to check. 
+	 * 
+	 * @return					true if SSN matches pattern, otherwise false.
+	 */
 	private static boolean isSocialSecurityNumber(String number) {
 		String pattern = "(?!666|000|9\\d{2})\\d{3}-"
 				+ "(?!00)\\d{2}-"
@@ -83,12 +103,29 @@ public class NamingConvention {
 	
 	//====================================================================================================
 	
+	/**
+	 * Returns boolean representation of whether or not email address  
+	 * matches convention.
+	 * 
+	 * @param eMail				the email to check. 
+	 * 
+	 * @return					true if email matches pattern, otherwise false.
+	 */
 	private static boolean isEmailSenderOrganization(String eMail) {
 		String pattern = "[a-zA-Z]{1}[\\.a-zA-Z0-9_-]+[a-zA-Z0-9]{1}@gmail.com";
 		return eMail.matches(pattern);
 	}
 	
 	//====================================================================================================
+	
+	/**
+	 * Returns boolean representation of whether or not S3 object prefix 
+	 * matches convention. 
+	 * 
+	 * @param prefix			the prefix to check. 
+	 * 
+	 * @return					true if prefix matches pattern, otherwise false.
+	 */
 	private static boolean isValidPrefix(String prefix) {
 		String pattern = "(land|prepare)/.+/DSET\\d{8}/.+";
 		return prefix.matches(pattern);
