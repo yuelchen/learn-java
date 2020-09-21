5d6c67f0454727a12b60314251202bb6c34e139f
yuelchen <ychen.workspace@gmail.com>
2020-09-20 21:52:11 -0400

    Added Java Documentation.

--- IntroExecutorService/src/com/yuelchen/threads/PrintRandomNumber.java
+++ IntroExecutorService/src/com/yuelchen/threads/PrintRandomNumber.java
@@ -4,24 +4,44 @@ import java.util.Random;
 
 public class PrintRandomNumber implements Runnable {
 
+	/**
+	 * Minimum range for random number.
+	 */
 	private int minRange = 0;
 	
 	//====================================================================================================
 	
+	/**
+	 * Maximum range for random number.
+	 */
 	private int maxRange = 10;
 	
 	//====================================================================================================
 	
+	 /**
+	  * Constructor
+	  */
 	public PrintRandomNumber() {}
 	
 	//====================================================================================================
 	
+	/**
+	 * Constructor 
+	 * 
+	 * @param max			the maximum range for random number.
+	 */
 	public PrintRandomNumber(int max) {
 		this.maxRange = max;
 	}
 	
 	//====================================================================================================
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param min			the minimum range for random number.
+	 * @param max			the maximum range for random number.
+	 */
 	public PrintRandomNumber(int min, int max) {
 		this.minRange = min;
 		this.maxRange = max;
@@ -29,6 +49,9 @@ public class PrintRandomNumber implements Runnable {
 	
 	//====================================================================================================
 	
+	/**
+	 * Run method for runnable class. 
+	 */
 	public void run() {
 		Random random = new Random();
 		System.out.println(String.format("Random number between '%d' and '%d': %d",
