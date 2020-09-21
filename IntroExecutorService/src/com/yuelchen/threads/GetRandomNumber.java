ec31861705da53cce204a5abc997847132a90371
yuelchen <ychen.workspace@gmail.com>
2020-09-20 21:52:02 -0400

    Added Java Documentation.

--- IntroExecutorService/src/com/yuelchen/threads/GetRandomNumber.java
+++ IntroExecutorService/src/com/yuelchen/threads/GetRandomNumber.java
@@ -3,26 +3,53 @@ package com.yuelchen.threads;
 import java.util.Random;
 import java.util.concurrent.Callable;
 
+/**
+ * Callable class for printing random number.
+ * 
+ * @author	yuelchen
+ * @version	1.0.0
+ * @since	2020-09-20
+ */
 public class GetRandomNumber implements Callable<Integer>{
 
+	/**
+	 * Minimum range for random number.
+	 */
 	private int minRange = 0;
 	
 	//====================================================================================================
 	
+	/**
+	 * Maximum range for random number.
+	 */
 	private int maxRange = 10;
 	
 	//====================================================================================================
-	
+
+	/**
+	 * Constructor.
+	 */
 	public GetRandomNumber() {}
 	
 	//====================================================================================================
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param max			the maximum range for random number.
+	 */
 	public GetRandomNumber(int max) {
 		this.maxRange = max;
 	}
 	
 	//====================================================================================================
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param min			the minimum range for random number.
+	 * @param max			the maximum range for random number.
+	 */
 	public GetRandomNumber(int min, int max) {
 		this.minRange = min;
 		this.maxRange = max;
@@ -30,6 +57,9 @@ public class GetRandomNumber implements Callable<Integer>{
 	
 	//====================================================================================================
 	
+	/**
+	 * Callable method for callable class.
+	 */
 	@Override
 	public Integer call() throws Exception {
 		Random random = new Random();
